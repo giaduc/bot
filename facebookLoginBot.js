57c81ab256fc87554880331475bd1d7e2c5982da
Lê gia Hiếu <legiahieu@Les-iMac.local>
2018-06-05 00:34:18 +0700

    update

--- facebookLoginBot.js
+++ facebookLoginBot.js
@@ -10,18 +10,18 @@ const {
  * auto login Facebook
  */
 (async function loginFacebook() {
-    const driver = new Builder().forBrowser('chrome').build();
+    const driver = new Builder().forBrowser('safari').build();
     driver.manage().window().maximize();
     try {
         await driver.get('https://facebook.com');
-        const email = driver.findElement(By.name('email'));
-        const password = driver.findElement(By.name('pass'));
-        await email.sendKeys('giaduc22@gmail.com');
-        await password.sendKeys('rDuLTDI9', Key.RETURN);
-        await driver.manage().getCookies().then((e) => {
-            const data = JSON.stringify(e, null, 2);
-            fs.writeFileSync('fbcookies.json', data, null);
-        });
+        // const email = driver.findElement(By.name('email'));
+        // const password = driver.findElement(By.name('pass'));
+        // await email.sendKeys('giaduc22@gmail.com');
+        // await password.sendKeys('rDuLTDI9', Key.RETURN);
+        // await driver.manage().getCookies().then((e) => {
+        //     const data = JSON.stringify(e, null, 2);
+        //     fs.writeFileSync('fbcookies.json', data, null);
+        // });
         await driver.manage().deleteAllCookies();
         await driver.navigate().refresh();
         const ob = readJsonFile();
--- googleSearchBot.js
+++ googleSearchBot.js
@@ -1,29 +1,23 @@
 const fs = require('fs');
-const {
-    Builder,
-    By,
-    Key,
-    until
-} = require('selenium-webdriver');
+const { Builder, By, Key, until } = require('selenium-webdriver');
 
 /**
  * auto Google search
  */
 (async function searchGoogle() {
-    const driver = new Builder().forBrowser('chrome').build();
+    const driver = new Builder().forBrowser('safari').build();
     driver.manage().window().maximize();
-    try {
-        await driver.get('https://google.com');
-        const element = driver.findElement(By.name('q'));
-        await element.sendKeys('Victory two step from hell', Key.RETURN);
-        await driver.takeScreenshot().then(e => {
-            const data = e.replace(/^data:image\/\w+;base64,/, "");
-            const buf = new Buffer(data, 'base64');
-            fs.writeFile('image.png', buf);
-        });
-    } catch (error) {
-        console.log(error);
-    } finally {
-        driver.quit();
-    }
+    await driver.get('https://google.com');
+    const element = driver.findElement(By.name('q'));
+    await element.sendKeys('Victory');
+    await element.submit();
+    driver.takeScreenshot().then(
+        (i, e) => {
+            const data = i.replace(/^data:image\/\w+;base64,/, "");
+            const buf = new Buffer.from(data, 'base64');
+            fs.writeFile('image.png', buf, e => {
+                if (e) throw e;
+            });
+        }
+    );
 })();
